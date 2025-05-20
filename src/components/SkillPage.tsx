a4beeee091a6ddc542dcd9ac50bad569ac882a86
Agorye Jessica <agoryejessica1@gmail.com>
2025-05-20 19:29:42 +0100

    fixed animation scroll

--- src/components/SkillPage.tsx
+++ src/components/SkillPage.tsx
@@ -5,7 +5,7 @@ import "aos/dist/aos.css";
 
 const SkillPage = () => {
   useEffect(() => {
-    AOS.init();
+    AOS.init({ once: true, duration: 1000 });
   }, []);
 
   const stack = ["MongoDB", "Express.js", "React", "Node.js"];
@@ -25,12 +25,18 @@ const SkillPage = () => {
       className="mt-16 grid grid-cols-1 md:grid-cols-2 gap-6 text-sm md:text-base rounded-tr-lg px-4"
     >
       {/* Stack Section */}
-      <div className="bg-gray-100 p-4 rounded-md shadow-md">
+      <div
+        data-aos="fade-up"
+        data-aos-delay="100"
+        className="bg-gray-100 p-4 rounded-md shadow-md"
+      >
         <p className="font-bold text-gray-800 mb-2">Stack: MERN</p>
         <ul className="flex flex-wrap gap-3 pl-2">
           {stack.map((tech, index) => (
             <li
               key={index}
+              data-aos="fade-up"
+              data-aos-delay={150 + index * 100}
               className="bg-blue-100 text-blue-800 px-3 py-1 rounded-full text-xs md:text-sm"
             >
               {tech}
@@ -40,7 +46,11 @@ const SkillPage = () => {
       </div>
 
       {/* Technologies Section */}
-      <div className="bg-gray-100 p-4 rounded-md shadow-md">
+      <div
+        data-aos="fade-up"
+        data-aos-delay="300"
+        className="bg-gray-100 p-4 rounded-md shadow-md"
+      >
         <div className="bg-black text-white px-3 py-1 rounded-md inline-block mb-2">
           <p className="text-sm md:text-base">Technologies</p>
         </div>
@@ -48,6 +58,8 @@ const SkillPage = () => {
           {technologies.map((tech, index) => (
             <li
               key={index}
+              data-aos="fade-up"
+              data-aos-delay={350 + index * 100}
               className="bg-green-100 text-green-800 px-3 py-1 rounded-full text-xs md:text-sm"
             >
               {tech}
